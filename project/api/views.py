8c58dba5691bf0435bec58ce632321c3f1740657
vivekkumar <vivek24ee066@satiengg.in>
2024-08-07 16:07:32 +0530

    thrid commit

--- project/api/views.py
+++ project/api/views.py
@@ -14,3 +14,19 @@ class StudentList(mixins.ListModelMixin,
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+    
+class StudentDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
