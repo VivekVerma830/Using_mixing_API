166b533cb11dff2d621c0e1994e6f3bb82c4551d
vivekkumar <vivek24ee066@satiengg.in>
2024-08-07 15:48:25 +0530

    second commit

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('student/', views.StudentList.as_view()),
+    # path('student/<int:pk>/', views.StudentDetail.as_view()),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-# Create your views here.
+class StudentList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
